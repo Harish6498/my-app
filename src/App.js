132b271424e473f2149a7970a85ec4de3e597b55
Harish6498 <hareesh6498@gmail.com>
2025-04-16 12:57:43 +0530

    add data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Input from './components/Input/Input';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <h2>This is React app</h2>
+    <Input/> */}
+    {/* <Header/> */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Header/>}/>
+        <Route path="/Input" element={<Input/>}/>
+
+
+        
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/components/Input/Input.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+export default function Input() {
+    const [userName,setUserName]=useState("");
+    const [password,setPassword]=useState("");
+    function getUserName(event){
+        console.log(event.target.value)
+        setUserName(event.target.value)
+    }
+    function getPassword(event){
+        console.log(event.target.value)
+        setPassword(event.target.value)
+    }
+    // function getDetailes(){
+
+    // }
+  return (
+    
+    <div>
+        <h2>this is input value</h2>
+        {/* {userName}
+        {password} */}
+        <label>UserName</label>
+        <input type="text" onChange={getUserName}/>
+        <label>password</label>
+        <input type="password" onChange={getPassword}/>
+        <button onClick={getDetailes}>submit</button>
+
+    </div>
+  )
+}
